fc028ca43ed11b1496b27b23e53a4961423b3b73
Michele Sato <michelesato2@hotmail.com>
2023-09-10 14:41:21 -0300

    Criação e configuração dos modelos de Despensa e ReceitaUsa

---
+++ projeto-6sem-temperalista/Model/Despensa.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace projeto_6sem_temperalista.Models
+{
+    [Table("Despensa")]
+    public class Despensa
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string ItemNome { get; set; }
+        [Required]
+        public string Marca { get; set; }
+        [Required]
+        public string Qtd { get; set; }
+        [Required]
+        public int CodBarras { get; set; }
+        
+        public ICollection<ReceitaUsa> ReceitaUsa { get; set; }
+
+    }
+}
---
+++ projeto-6sem-temperalista/Model/ReceitaUsa.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Security.Principal;
+
+namespace projeto_6sem_temperalista.Models
+{
+    [Table("ReceitaUsa")]
+    public class ReceitaUsa
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string ItemReceita { get; set; }
+        [Required]
+        public string ItemQtd { get; set; }
+        [Required]
+        public Tipo_Item Tipo { get; set; }
+        [Required]
+        public int DespensaId { get; set; }
+        [Required]
+        public Despensa Despensa { get; set; }
+    }
+    public enum Tipo_Item
+    {
+        Supermercado,
+        Feira
+    }
+}
